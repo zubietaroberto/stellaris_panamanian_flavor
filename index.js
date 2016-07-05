3fcec7f507f83568165dd1cd86956efe671d98db
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2016-07-05 13:59:01 -0500

    Added support for multiple namefiles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require("lodash");
 var mu = require("mu2");
+var Promise = require('bluebird');
 
 // Basic constants
 mu.root = __dirname + '/templates'
@@ -11,38 +12,80 @@ var output_folder = path.join(__dirname, 'outputs');
 var output_file = path.join(output_folder, 'Panamanian.txt');
 
 // Filepaths
-var nl_panama_city_neighborhoods = path.join(__dirname, 'namelists/panama_city_neighborhoods.txt');
+var filenames = [
+  {
+    path: path.join(__dirname, 'namelists/panama_city_neighborhoods.txt'),
+    name:'ship_corvettes'
+  },
+  {
+    path: path.join(__dirname, 'namelists/panama_city_streets.txt'),
+    name:'ship_destroyers'
+  }
+]
 
-// Read the File Paths
-var panama_city_neighborhoods = fs
-  .readFileSync(nl_panama_city_neighborhoods, {encoding:'UTF-8'});
+Promise
+  .all(filenames)
 
-// Parse names
-var output = _(panama_city_neighborhoods.split(/\r\n|\r|\n/))
+  // Transform each filename into a namelist
+  .map( filename => {
+    return Promise
+      .fromCallback(cb => fs.readFile(filename.path, {encoding:'UTF-8'}, cb))
+      .then(raw_file => {
+        var parsed_items = _(raw_file.split(/\r\n|\r|\n/))
+          // Remove Empty
+          .reject(item_name  => _.isEmpty(item_name))
+        return {list: parsed_items, name:filename.name }
+      })
+      ;
+  })
 
-  // Remove empty
-  .reject( item_name  => _.isEmpty(item_name))
+  //Accumulate namelists
+  .reduce( (accumulator, parsed_file) => {
+    if (!_(accumulator).has(parsed_file.name)){
+      accumulator[parsed_file.name] = parsed_file.list;
+    } else {
+      accumulator[parsed_file.name].push(parsed_file.list);
+    }
+    return accumulator;
+  }, {})
 
-  //Reduce to lines of eight
-  .reduce( (out_array, item_name, counter) => {
+  .then(struct => {
+    for(var prop in struct){
 
-    // Every eight items, add a new line
-    if (counter%items_per_line == 0){
-      out_array.push("\t\t\t");
+      // Parse names
+      var output = _(struct[prop])
+
+        //Reduce to lines of eight
+        .reduce( (out_array, item_name, counter) => {
+
+          // Every eight items, add a new line
+          if (counter%items_per_line == 0){
+            out_array.push("\t\t\t");
+          }
+
+          out_array[Math.floor(counter/items_per_line)] += `"${item_name}" `;
+          return out_array;
+        }, [])
+
+        // Add Line Breaks
+        .join("\n")
+        ;
+
+      struct[prop] = output;
     }
 
-    out_array[Math.floor(counter/items_per_line)] += `"${item_name}" `;
-    return out_array;
-  }, [])
+    return struct;
 
-  // Add Line Breaks
-  .join("\n")
-  ;
+  })
 
-// Output file from the template using Mustache
-mu
-  .compileAndRender('namelist.mustache', {ship_corvettes: output})
-  .on('data', data => {
-    console.log(data.toString());
+  // Output file from the template using Mustache
+  .then(struct => {
+    mu
+      .compileAndRender('namelist.mustache', struct)
+      .on('data', data => {
+        console.log(data.toString());
+      })
+      ;
+
   })
   ;
