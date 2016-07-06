210b5c2eee50b615f91b7388b2d316a4c5e235fe
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2016-07-06 14:47:39 -0500

    Added regnal character names

--- index.js
+++ index.js
@@ -103,16 +103,28 @@ var filenames = [
   },
 
   {
-    path:'name_male.txt',
+    path:'names_male.txt',
     name:'names_male',
   },
   {
-    path:'name_female.txt',
+    path:'names_female.txt',
     name:'names_female',
   },
   {
     path:'surnames.txt',
     name:'surnames',
+  },
+  {
+    path:'regnal_male.txt',
+    name:'regnal_male',
+  },
+  {
+    path:'regnal_female.txt',
+    name:'regnal_female',
+  },
+  {
+    path:'regnal_surnames.txt',
+    name:'regnal_surnames',
   },
 ]
 
