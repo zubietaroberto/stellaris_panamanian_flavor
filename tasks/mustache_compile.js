e3413d21c7a723ebb7eda2c20cd946c8f4ba7cac
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2016-09-20 10:54:17 -0500

    Using grunt internal APIs for file processing

--- tasks/mustache_compile.js
+++ tasks/mustache_compile.js
@@ -1,60 +1,64 @@
 const mu = require("mu2")
 const Promise = require("bluebird")
-const fs = Promise.promisifyAll(require('fs-extra'))
-const path = require('path')
-const streamToPromise = require("stream-to-promise")
 const _ = require('lodash')
 
 module.exports = function(grunt) {
 
-    grunt.registerTask('mustache_compile', function(){
+    grunt.registerMultiTask('mustache_compile', function(){
 
+        // Get Grunt Options
+        let task_options = this.options()
+        let output_array = this.files
+
+        // Generator Coroutine function
+        let variables_generator = function*(){
+
+            // Merge all variables
+            let dynamic_variables = yield task_options.dynamic_variables()
+            let variables = _.merge(task_options.variables, dynamic_variables)
+
+            // Define result array
+            let result_destinations = []
+
+            mu.root = process.cwd()
+            for (mapping of output_array){
+                for (source of mapping.src){
+
+                    // Start rendering the file to a stream
+                    let render_stream = mu.compileAndRender(source, variables)
+                    let destination_array = _.isString(mapping.dest) ? 
+                        [mapping.dest] : mapping.dest;
+
+                    for (destination of destination_array){
+                        // Output the stream 
+                        grunt.file.write(destination, render_stream)
+
+                        // Logs
+                        grunt.log.ok(destination)
+                        result_destinations.push(destination)
+                    }
+
+                }
+            }
+
+            // Result
+            return result_destinations
+        }
+
+        /*
+            Main Execution Chain
+        */
         let done = this.async()
-        let mu_variables = grunt.config('mustache_compile').variables
-        let dynamic_variables = grunt.config('mustache_compile').dynamic_variables
-        let files = grunt.config('mustache_compile').files
-
         Promise
-            .try(dynamic_variables)
-            // Add the variables to the data
-            .then( data => {
-                let variables = _.merge(data, mu_variables)
-                return Promise
-                    .all(files)
-                    // Start compiling the file
-                    .map(mapping => {
-                        mu.root = process.cwd()
-                        mapping["renderStream"] = mu.compileAndRender(mapping.src, variables)
-                        return mapping
-                    })
-            })
-            .map(mapping => {
-                // Normalize destinations: must always be an array of strings
-                let rel_destination_array = (_.isString(mapping.dest)) ?
-                    [mapping.dest] : mapping.dest
-
-                return Promise
-                    .all(rel_destination_array)
-                    .map(rel_destination => {
-
-                        // Get the Absolute Path
-                        let destination =  path.join(process.cwd(), rel_destination)
-                        let array = destination.split("/") 
-                        let dir = array.splice(0, array.length -1).join("/")
-
-                        // Make sure directories exist
-                        return fs.ensureDirAsync(dir).then(() => (destination))
-                    })
-                    // Create files
-                    .map(destination => {
-                        let render_stream = mapping.renderStream
-                        let outputStream = fs.createWriteStream(destination, {encoding:'UTF-8'})
-                        return streamToPromise(render_stream.pipe(outputStream))
-                            .then(() => grunt.log.ok(destination))
-                    })
-            })
+            // Execute the Generator as a Coroutine
+            .try(Promise.coroutine(variables_generator))
             // finalize
-            .finally(done)
+            .then(done)
+            // Catch any error
+            .catch( error => {
+                grunt.log.error(error)
+                done(error)
+            })
     })
 
 }
