3161ceddc469fba40306579f25a6e1a84004f546
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2016-09-23 13:54:30 -0500

    Simplified YAML mapping

--- generator/index.js
+++ generator/index.js
@@ -11,67 +11,62 @@ var items_per_line = 8;
 // Coroutine that feeds names from a namefile
 const name_feeder_coroutine = Promise.coroutine(function*(query){
 
-  // Result
-  let parsed_items = []
-  for(rel_path of query.namelists){
+  let result_dictionary = {}
+  for (prop in query){
 
-    //Read the file
-    let filepath = path.join(process.cwd(), `namelists/${rel_path}`) 
-    let raw_file = yield fs.readFileAsync(filepath, {encoding:'UTF-8'})
+    // Iterate through the dictionary 
+    let property = query[prop]
+    let parsed_items = []
+    for(rel_path of property.namelists){
 
-    // Parse the names
-    let names = raw_file.split(/\r\n|\r|\n/)
-        // Remove Empty
-        .filter( item_name    => !_.isEmpty(item_name))
-        // Remove Commented
-        .filter( item_name    => !_.startsWith(item_name, "#"))
-        // Add prefix, if it exists
-        .map( item_name       => (!_.isEmpty(query.prefix) ?
-          `${query.prefix} ${item_name}` : item_name
-        ))
+      //Read the file
+      let filepath = path.join(process.cwd(), `namelists/${rel_path}`) 
+      let raw_file = yield fs.readFileAsync(filepath, {encoding:'UTF-8'})
 
-    // Add the names to the accumulator
-    _.merge(parsed_items, names)
+      // Parse the names
+      let names = raw_file.split(/\r\n|\r|\n/)
+          // Remove Empty
+          .filter( item_name    => !_.isEmpty(item_name))
+          // Remove Commented
+          .filter( item_name    => !_.startsWith(item_name, "#"))
+          // Add prefix, if it exists
+          .map( item_name       => (!_.isEmpty(property.prefix) ?
+            `${property.prefix} ${item_name}` : item_name
+          ))
+
+      // Add the names to the accumulator
+      _.merge(parsed_items, names)
+    }
+
+    // Return the mapping
+    result_dictionary[prop] = parsed_items
   }
 
-  // Return the mappping
-  return {list: parsed_items, name:query.name }
+  return result_dictionary
+
 })
-
-// Merges all filenames into a single dictionary
-const name_accumulator = function(accumulator, parsed_file){
-  if (!_(accumulator).has(parsed_file.name)){
-    accumulator[parsed_file.name] = parsed_file.list;
-  } else {
-    var new_array = _.concat(accumulator[parsed_file.name], parsed_file.list);
-    accumulator[parsed_file.name] = new_array;
-  }
-  return accumulator;
-};
 
 // Merges names into rows
 const row_accumulator = function(struct){
+  let result_dictionary = {}
   for(var prop in struct){
 
     // Parse names
-    var output = _(struct[prop])
-
+    let output = struct[prop]
       //Join into lines
       .reduce( (out_array, item_name, counter) => {
         if (counter%items_per_line == 0){
           // Add a new line
-          out_array.push("\t\t\t");
+          out_array.push("");
         }
         out_array[Math.floor(counter/items_per_line)] += `"${item_name}" `;
         return out_array;
       }, [])
-
       // Add Line Breaks
-      .join("\n")
-      ;
-    struct[prop] = output;
+      .join("\n\t\t\t")
+    result_dictionary[prop] = output;
   }
-  return struct;
+  return result_dictionary;
 };
 
 // Coroutine that loads the YAML Mapping
@@ -92,10 +87,7 @@ module.exports = function(){
     .try(load_mapping_coroutine)
 
     // Transform each filename into a namelist
-    .map(name_feeder_coroutine)
-
-    // Merge filelists into a single dictionary
-    .reduce(name_accumulator, {})
+    .then(name_feeder_coroutine)
 
     // Organize elements in rows
     .then(row_accumulator)
